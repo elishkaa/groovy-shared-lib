6a657fb59c7d21404514ede27e8c43602a1b25b1
elishkaa <eliran.avihen@gmail.com>
2024-09-03 10:58:03 +0300

    add send email method

---
+++ vars/sendEmail.groovy
@@ -0,0 +1,21 @@
+def call(Map config = [:]) {
+    def emailData = [
+                        email: ${config.TO},
+                        subject: ${config.SUBJECT},
+                        message: ${config.MESSAGE}
+                    ]
+                    def emailDataJson = groovy.json.JsonOutput.toJson(emailData)
+
+                    sh """
+                        curl -X POST http://host.docker.internal:8000/send-email/ \
+                        -H 'Content-Type: application/json' \
+                        -d '${emailDataJson}'
+                    """
+    
+    def isSuccess = true
+    if (apiResponse != "200") {
+        isSuccess = false
+    }
+
+    return isSuccess
+}
